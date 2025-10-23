059ed709d1314c4f11361fb7335e93fb6de0254c
cmarinoa <cmarinoa25@fpcoruna.afundacion.org>
2025-10-23 10:21:48 +0200

    Ejercicio 10 - Scrollbar

---
+++ sprint1tkinter/ejercicio10_Scrollbar.py
@@ -0,0 +1,87 @@
+import tkinter as tk
+
+def insertar_texto():
+        cuadro_texto.insert("1.0", """EN	 UN	 LUGAR	 de	 la	 Mancha,	 de	 cuyo	 nombre	 no	 quiero	 acordarme,					
+no	 ha	 mucho	 tiempo	 que	 vivía	 un	 hidalgo	 de	 los	 de	 lanza	 en	 astillero,	 adarga	
+antigua,	 rocín	 flaco	 y	 galgo	 corredor.	 Una	 olla	 de	 algo	 mas	 vaca	 que	 carnero,	
+salpicón	las	más	 noches,	 duelos	 y	 quebrantos	los	 sábados,	lantejas	los	 viernes,	
+algún	 palomino	 de	 añadidura	 los	 domingos,	 consumían	 las	 tres	 partes	 de	 su	
+hacienda.	El	resto	della	concluían	sayo	de	velarte,	y	los	días	de	entresemana	se
+honraba	con	su	vellorí	de	lo	más	fino. Tenía	en	su	casa	una	ama	que	pasaba	de	
+los	cuarenta,	y	una	sobrina	que	no	llegaba	a	los	veinte.	Frisaba	la	edad	de	nuestro	
+hidalgo	con	los	cincuenta	años;	era	de	complexión	recia,	seco	de	carnes,	enjuto	
+de	 rostro,	 gran	 madrugador	 y	 amigo	 de	 la	 caza.	 Quieren	 decir	 que	 tenía	 el	
+sobrenombre	de	Quijada,	o	Quesada,	aunque	por	conjeturas	verosímiles	se	deja	
+entender	que	se	llamaba	Quejana.
+Es,	pues,	de	saber,	que	este	sobredicho	hidalgo	los	ratos	que	estaba	ocioso	
+(que	eran	los	más	del	año)	se	daba	a	leer	libros	de	caballerías	con	tanta	afición	y	
+gusto,	 que	 olvidó	 casi	 de	 todo	 punto	 el	 ejercicio	 de	 la	 caza	 y	 aún	 la	
+administración	de	su	hacienda;	y	llegó	a	 tanto	su	curiosidad y	desatino en	esto,	
+que	 vendió	 muchas	 hanegas	 de	 tierra	 de	 sembradura	 para	 comprar	 libros	 de	
+caballerías	 en	 que	leer;	 y	 de	 todos,	 ninguno	le	 parecían	 tan	 bien	 como	los	 que	
+compuso	 el	 famoso	 Feliciano	 de	 Silva;	 y	 más	 cuando	 llegaba	 a	 leer	 aquellos	
+requiebros	 y	 cartas	 de	 desafíos	 donde	 en	 muchas	 partes	 hallaba	 escrito:	 “	 La	
+razón	de	la	sinrazón	que	a	mi	razón	se	hace,	de	 tal	manera	mi	razón	enflaquece,	
+que	con	razón	me	quejo	de	la	vuestra	fermosuta”.
+En	 resolución,	 él	 se	 enfrascó	 tanto	 en	 su	 lectura,	 que	 se	 le	 pasaban	 las	
+noches	leyendo	de	claro	en	claro,	 y	los	días	de	 turbio	en	 turbio;	 y	así,	del	poco	
+dormir	 y	 del	mucho	leer	 se	le	 secó	 el	 cerebro	 de	manera	 que	 vino	 a	 perder	 el	
+juicio.	 Llenósele	 la	 fantasía	 de	 todo	 aquello	 que	 leía	 en	 los	 libros,	 así	 de	
+encantamientos	 como	 de	 pendencias,	 batallas,	 desafíos,	 heridas,	 requiebros,	
+amores,	tormentas	y	disparates	imposibles.
+En	 efecto,	 rematado	 ya	 su	 juicio,	 vino	 a	 dar	 en	 el más	 extraño	
+pensamiento	que	jamás	dio	loco	en	el	mundo,	y	 fue	que	le	pareció	convenible	y	
+necesario,	así	para	el	aumento	de	su	honra	como	para	el	servicio	de	su república,	
+hacerse	 caballero	 andante.	 Y	 lo	 primero	 que	 hizo	 fue	 limpiar	 unas	 armas	 que	
+habían	 sido	 de	 sus	 bisabuelos,	 que,	 tomadas	 de	 orín	 y	llenas	 de	moho,	luengos	
+siglos	había	que	estaban	puestas	y	olvidadas	en	un	rincón.	
+Fue	 luego	 a	 ver	 su	 rocín,	 y	 aunque	 tenía	 mas	 cuartos	 que	 un	 real,	 le	
+pareció	que	ni	el	Bucéfalo	de	Alejandro	ni	Babieca	el	del	Cid	con	él	se	igualaban.	
+Cuatro	 días	 se	 le	 pasaron	 en	 imaginar	 qué	 nombre	 le	 pondría;	 después	 de	
+muchos	nombres	que	formó,	borró	y	quitó,	añadió,	deshizo	y	tornó	a	hacer	en	su	
+memoria	e	imaginación,	al	fin	le	vino	a	llamar	Rocinante.
+Puesto	nombre,	y	tan	a	su	gusto,	a	su	caballo,	quiso	ponérsele	a	sí	mismo,	
+y	 en	 este	 pensamiento	 duró	 otros ocho días,	 y	 al	 cabo	 se	 vino	 a	 llamar	 don	
+Quijote.	Pero	acordándose	 que	el	 valeroso	Amadís	no	 solo	 se	 había	 contentado	
+con	llamarse	Amadís	a	secas,	sino	que	añadió	el	nombre	de	su	reino	y	patria,	por	
+hacerla	 famosa,	 y	 se	 llamó	 Amadís	 de	 Gaula,	 así	 quiso,	 como	 buen	 caballero,	
+añadir	al	suyo	el	nombre	de	la	suya	y	llamarse	don	Quijote	de	la	Mancha.
+Limpias,	pues,	las	armas,	puesto	nombre	a	su	rocín	y	confirmándose	a	sí	
+mismo,	se	dio	a	entender	que	no	le	faltaba	otra	cosa	sino	que	buscar	una	dama	de	
+quien	enamorarse;	porque	el	caballero	andante	sin	amores	era	árbol	sin	hojas	y	
+sin	fruto	y	cuerpo	sin	alma.
+Y	 fue,	 a	 lo	 que	 se	 cree,	 que	 en	 un	 lugar	 cerca	 del	 suyo	 había	 una	 moza	
+labradora	 de	 muy	 buen	 parecer,	 de	 quien	 él	 un	 tiempo	 anduvo	 enamorado,	
+aunque,	 según	 se	 entiende,	 ella	 jamás	 lo	 supo	 ni	 le	 dio	 cata	 dello.	 Llamábase	
+Aldonza	 Lorenzo,	 y	 a	 esta	 le	 pareció	 ser	 bien	 darle	 título	 de	 señora	 de	 sus	
+pensamientos;	 y,	 buscándole	 nombre	 que	 no	 desdijese	 mucho	 del	 suyo	 y	 que	
+tirase	y	se	encaminase	al	de	princesa	y	gran	señora,	vino	a	llamarla	Dulcinea	del	
+Toboso,	 porque	 era	 natural	 del	 Toboso:	 nombre,	 a	 su	 parecer,	 músico	 y	
+peregrino	 y	 significativo,	 como	 todos	 los	 demás	 que	 a	 él	 y	 a	 sus	 cosas	 había	
+puesto.
+        """)
+
+root = tk.Tk()
+root.title("Ejercicio 10: Scrollbar")
+root.geometry("400x400")
+
+frame = tk.Frame(root)
+frame.pack(fill="both", expand=True)
+
+cuadro_texto = tk.Text(frame, wrap='none')
+cuadro_texto.grid(row=0, column=0, sticky="nsew")
+
+scrollbar_vert = tk.Scrollbar(frame, orient="vertical", command=cuadro_texto.yview)
+scrollbar_vert.grid(row=0, column=1, sticky="ns")
+cuadro_texto.config(yscrollcommand=scrollbar_vert.set)
+
+scrollbar_hor = tk.Scrollbar(frame, orient="horizontal", command=cuadro_texto.xview)
+scrollbar_hor.grid(row=1, column=0, sticky="ew")
+cuadro_texto.config(xscrollcommand=scrollbar_hor.set)
+
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+
+insertar_texto()
+
+root.mainloop()
