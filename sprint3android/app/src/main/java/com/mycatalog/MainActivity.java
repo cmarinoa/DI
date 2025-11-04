a654937a5ce5deca179a71599815a9259a77f1b7
cmarinoa <cmarinoa@fpcoruna.afundacion.org>
2025-11-04 19:30:43 +0100

    Ejercicio 2: Creado botón detailButton y lanzada actividad DetailActivity desde MainActivity(CatalogActivity)

--- sprint3android/app/src/main/java/com/mycatalog/MainActivity.java
+++ sprint3android/app/src/main/java/com/mycatalog/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button detailButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        }
+
+        detailButton = findViewById(R.id.detailButton);
+
+        // Configurar el click correctamente
+        detailButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            startActivity(intent);
+        });
     }
+}
