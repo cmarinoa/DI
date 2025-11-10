7599c542a0f260c526f64236a046268e4a84e74e
cmarinoa <cmarinoa25@fpcoruna.afundacion.org>
2025-11-10 16:17:53 +0100

    Corrección del ImageView circular. Código modificado para que aparezca la imagen redonda en vez de cuadrada.

--- sprint3android/app/build.gradle.kts
+++ sprint3android/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.navigation:navigation-fragment:2.5.3")
     implementation("androidx.navigation:navigation-ui:2.5.3")
     testImplementation(libs.junit)
--- sprint3android/app/src/main/java/com/mycatalog/DetailFragment.java
+++ sprint3android/app/src/main/java/com/mycatalog/DetailFragment.java
@@ -15,7 +15,7 @@ import androidx.fragment.app.Fragment;
 public class DetailFragment extends Fragment {
 
     public DetailFragment() {
-        // Constructor vacío requerido
+
     }
 
     @Nullable
